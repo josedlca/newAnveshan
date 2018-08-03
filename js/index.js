3e94b17183f4377cfe304839b09ee1998635553d
Your Name <you@example.com>
2018-08-03 12:20:32 -0500

    closeNav and rezise video

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-// let popupLink = document.querySelector('.popup__link');
-// let popup = document.getElementById('popup');
-// let popupClose = document.querySelector('.popup__close');
+
 let homeMenu = document.getElementById('home-menu');
 let menu = document.querySelector('.menu');
 let myMenuClose = document.querySelector('.iconmenu');
+let closeHome = document.querySelector('.close-home');
+let closeQuisque = document.querySelector('.close-quisque');
+let closeProcess = document.querySelector('.close-process');
+let closeServices = document.querySelector('.close-services');
+let closeStep = document.querySelector('.close-step');
+let closeFooter = document.querySelector('.close-footer');
+let closeContact = document.querySelector('.close-contact');
 
 homeMenu.addEventListener('click', function(){
     menu.classList.add('menu-see');
@@ -13,11 +18,16 @@ myMenuClose.addEventListener('click', function(){
     menu.classList.remove('menu-see');
 });
 
+function closeNav(section){
+    section.addEventListener('click', function(){
+        menu.classList.remove('menu-see');
+    });
+}
 
-// popupLink.addEventListener('click',function(){
-//     popup.classList.add('mostrar');
-// }); 
-
-// popupClose.addEventListener('click',function(){
-//     popup.classList.remove('mostrar');
-// });
+closeNav(closeHome);
+closeNav(closeQuisque);
+closeNav(closeProcess);
+closeNav(closeServices);
+closeNav(closeStep);
+closeNav(closeFooter);
+closeNav(closeContact);
